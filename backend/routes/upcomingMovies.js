3e8f7a6096b631e39145decd14a8507c0e0ec1b3
Justyna <justyna.chmielinska@coderscrew.pl>
2022-03-01 18:24:05 +0100

    feat:carousel of upcoming movies

--- backend/routes/upcomingMovies.js
+++ backend/routes/upcomingMovies.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 const Movie = require('../model/Movie');
+const cors = require("cors");
 
 const currentYear = new Date().getFullYear()  // returns the current year
 
-router.get('/upcoming', async (req, res) => {
+router.get('/upcoming', cors(), async (req, res) => {
     const filter = { year: { $gte: currentYear } };
     const allUpcoming = await Movie.find(filter);
 
